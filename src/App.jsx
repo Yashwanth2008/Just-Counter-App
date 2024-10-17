af400c76cf0b3b4b802f2e86e07477bbccccd6ae
Yashwanth2008 <vyashwanth2003@gmail.com>
2024-10-17 09:58:26 +0530

    Reset functionality

--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,19 @@ function App() {
       return value;
     });
   };
+
+  const handleReset = () => {
+    setNum((value) => {
+      return value = 0;
+    })
+  }
   return (
     <div className="overall-wrapper">
       <div className="icons">
-        <i class="fa-solid fa-circle-half-stroke"></i>
-        <i class="fa-solid fa-circle-info"></i>
-        <i class="fa-solid fa-gear"></i>
-        <i class="fa-solid fa-rotate-right"></i>
+        <i className="fa-solid fa-circle-half-stroke"></i>
+        <i className="fa-solid fa-circle-info"></i>
+        <i className="fa-solid fa-gear"></i>
+        <i className="fa-solid fa-rotate-right" onClick={handleReset}></i>
       </div>
       <div className="content">
         <button id="addBtn" onClick={handleDecrement}>
@@ -31,7 +37,7 @@ function App() {
         </button>
         <h1>{num}</h1>
         <button id="subBtn" onClick={handleIncrement}>
-          <i class="fa-solid fa-plus"></i>
+          <i className="fa-solid fa-plus"></i>
         </button>
       </div>
       <div className="data">
